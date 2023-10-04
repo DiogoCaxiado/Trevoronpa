0f2a6c9cdab8567dd5be5a36f60eae8dc7062f59
DIOGO EVANGELISTA CAXIADO <diogo.ecaxiado@senacsp.edu.br>
2023-10-04 11:44:13 -0300

    mobile funcionando

--- js/script.js
+++ js/script.js
@@ -9,17 +9,26 @@ function handleMenu() {
     const menu = document.querySelector(".menu");
     const opacity = document.querySelector(".menu__opacity");
     const container = document.querySelectorAll(".container");
+    const header = document.querySelector("header");
+    const footer = document.querySelector("footer");
+    const card = document.querySelector(".cards");
 
     if (menu.style.display == "none") {
         menu.style.display = "block";
         opacity.style.backgroundColor = "rgba(0, 0, 0, 0.60)";
-  
+        header.style.zIndex = "-1";
+        footer.style.zIndex = "-1";
+        card.style.zIndex = "-1";
+     
         for (const item of container) {
-            item.style.zIndex = "-1";
+            item.style.zIndex = "-2";
         }
     } else {
         menu.style.display = "none";
         opacity.style.backgroundColor = "transparent";
+        header.style.zIndex = "2";
+        footer.style.zIndex = "2";
+        card.style.zIndex = "1";
 
         for (const item of container) {
             item.style.zIndex = "1";
